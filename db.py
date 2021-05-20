a88b038be1565b36a04ae07134b70e80ab554bc7
abinandanshetty <84447639+abinandanshetty@users.noreply.github.com>
2021-05-20 22:47:18 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,16 @@
+while(True):
+            input1 = input("Enter your name:")
+            input2 = input("Enter your phone number:")
+            input3 =input("Enter your place of coming:")
+            input4=input("Enter your body temperature:")
+            
+
+
+            file = open("data.txt", "a+")
+
+            file.write(input1 + " " + input2 +" " +input3 + " " + input4 + "\n")
+            #appending the input for the file
+            
+            file.close()
+            continue
+
